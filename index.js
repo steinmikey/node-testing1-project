bb20bb1b3b5b8e06ccb5d1beadd08055bbbd7fb2
Michael Steiner <michaelvsteiner@gmail.com>
2021-10-14 04:39:35 -0600

    completes exercise 7

--- index.js
+++ index.js
@@ -184,8 +184,11 @@ class Car {
  *    // result is false
  * })
  */
-function isEvenNumberAsync(number) {
-  // ✨ implement
+async function isEvenNumberAsync(number) {
+  if (number % 2 === 0) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = {
